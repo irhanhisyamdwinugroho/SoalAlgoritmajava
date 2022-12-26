63132db9ccf4aa0dfc96f7733cc2c9e8f4d7bf04
Irhan Hisyam <108678365+irhanhisyamdwinugroho@users.noreply.github.com>
2022-12-27 03:01:20 +0800

    penambahan ganjil genap

---
+++ src/ganjilgenap.java
@@ -0,0 +1,12 @@
+public class ganjilgenap {
+    public static void main(String[] args) {
+
+
+        int angka = 5;
+        if (angka % 2 == 0) {
+            System.out.println("Angka " + angka + " adalah bilangan genap");
+        } else {
+            System.out.println("Angka " + angka + " adalah bilangan ganjil");
+        }
+    }
+}
